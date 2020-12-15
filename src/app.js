acf8ddb17e4b26c49395a4465c00859d458a3aca
Pushpakant Behera <pushpakantbehera@hotmail.com>
2020-12-15 23:12:58 +0545

    ready for heroku

--- src/app.js
+++ src/app.js
@@ -14,37 +14,17 @@ const app = express();
 //  middlewares
 app.use(helmet());
 
-app.use(express.static('dist/typeracer-clone'));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname));
-});
-
-// app.get('/', (req, res) => {
-//   res.send('Express is working!');
-// });
-
 app.use(middlewares.notFound);
 
 app.use(middlewares.errorHandler);
 
-const port = 80;
+const port = process.env.PORT || 80;
 
 const server = app.listen(port, () => {
   console.log('Server Listening at port ', port);
 }); //  returns http object
 
 const io = socketio(server); //  passing express-server object to socketio server
-
-//  connecting to mongodb locally
-
-// mongoose.connect(
-//   'mongodb://localhost/typeracer-clone',
-// { useNewUrlParser: true, useUnifiedTopology: true },
-//   () => {
-//     console.log('Connected to database!');
-//   }
-// );
 
 //  connecting to mongodb atlas
 mongoose.connect(
@@ -54,5 +34,11 @@ mongoose.connect(
     console.log('Connected to database!');
   }
 );
+
+app.use(express.static('dist/typeracer-clone'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname));
+});
 
 io.on('connect', socketIoHandler(io));
