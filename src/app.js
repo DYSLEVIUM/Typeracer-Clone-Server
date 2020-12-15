7fb5df90b54474965fbf0d6f3893f75bc359c3a5
Pushpakant Behera <pushpakantbehera@hotmail.com>
2020-12-15 22:53:45 +0545

    ready for heroku

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use(middlewares.notFound);
 
 app.use(middlewares.errorHandler);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
   console.log('Server Listening at port ', port);
